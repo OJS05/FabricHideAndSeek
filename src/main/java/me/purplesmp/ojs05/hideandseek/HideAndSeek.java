0b00e4b4c4cc8f762871c6e7a51348d4dcde5758
Oliver Shearman <bygollyitsolli@gmail.com>
2021-07-16 21:45:11 +1000

    init

---
+++ src/main/java/me/purplesmp/ojs05/hideandseek/HideAndSeek.java
@@ -0,0 +1,10 @@
+package me.purplesmp.ojs05.hideandseek;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HideAndSeek implements ModInitializer {
+    @Override
+    public void onInitialize(){
+
+    }
+}
